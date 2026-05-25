9560acf6a05272029b773de7e2539baa6ffa1063
anaflo <38280550+anaflom@users.noreply.github.com>
2026-05-25 14:42:38 +0200

    comment pipeline

--- script_pipeline_preprocessing.py
+++ script_pipeline_preprocessing.py
@@ -38,6 +38,10 @@ def main():
 
     # Preprocessing parameters
     # ============================================================================================
+    # The order of the steps peformed in the preprocessing pipeline is the following:
+    # 1. Initial steps: croping, resampling, filtering, picking electrodes, setting the montage (if specified)
+    # 2. ICA (if specified)
+    # 3. APICE preprocessing steps: bad channels detection, artifacts detection, local correction of artifacts using PCA and spline methods, etc. with the specified configurations for each step (or the default configurations if not specified).
 
     # List of electrodes to drop (e.g., dead channels, etc.) 
     DROP_ELECTRODES = ['E125','E126','E127','E128']
@@ -73,17 +77,6 @@ def main():
     L_FREQ_INITIAL = 0.10
     H_FREQ_INITIAL = 40
     LINE_NOISE_FREQ_INITIAL = None # Frequency for line noise removal (set to None to not apply line noise removal). The harmonics of the line noise frequency will also be removed up to the Nyquist frequency.
-    
-    # Filtering parameters for the APICE preprocessing steps (set to None to not apply band-pass filtering). L_FREQ_APICE should be set to a value >0 to apply the high-pass filtering in the APICE default pipeline, which is required for filtering after local artifacts correction steps.
-    L_FREQ_APICE = 0.10
-    H_FREQ_APICE = None
-    LINE_NOISE_FREQ_APICE = None
-
-    # Filter parameters to apply for artifact detection steps. 
-    # A copy of the data will be filtered with these parameters and used only for artifact detection steps (e.g., bad channels detection, artifacts detection, etc.) to improve the detection of artifacts. 
-    # Set to None to not apply specific filtering for artifact detection and use the same filtered data as for the rest of preprocessing steps (i.e., l_freq, h_freq, etc.). 
-    L_FREQ_ARTIFACTS = None
-    H_FREQ_ARTIFACTS = None
 
     # Parameter for ICA if applied
     APPLY_ICA = True
@@ -103,6 +96,17 @@ def main():
         'iclabel_lim_probability':0.9, # Probability threshold for labeling ICA components as artifacts using the ICLabel algorithm. Components with a probability of being an artifact above this threshold will be labeled as artifacts. 
         'iclabel_labels_to_exclude': ['eye blink', 'muscle artifact', 'heart beat', 'line noise', 'channel noise'], # List of ICLabel labels to consider as artifacts and exclude. The possible labels are: 'eye blink', 'eye movement', 'muscle artifact', 'heart beat', 'line noise', 'channel noise', 'other'. 
     }
+   
+    # Filtering parameters for the APICE preprocessing steps (set to None to not apply band-pass filtering). L_FREQ_APICE should be set to a value >0 to apply the high-pass filtering in the APICE default pipeline, which is required for filtering after local artifacts correction steps.
+    L_FREQ_APICE = 0.10
+    H_FREQ_APICE = None
+    LINE_NOISE_FREQ_APICE = None
+
+    # Filter parameters to apply for artifact detection steps. 
+    # A copy of the data will be filtered with these parameters and used only for artifact detection steps (e.g., bad channels detection, artifacts detection, etc.) to improve the detection of artifacts. 
+    # Set to None to not apply specific filtering for artifact detection and use the same filtered data as for the rest of preprocessing steps (i.e., l_freq, h_freq, etc.). 
+    L_FREQ_ARTIFACTS = None
+    H_FREQ_ARTIFACTS = None
 
     # Set to None to apply the default configuration.
     CFG_BAD_CHANNELS_DETECTION = None
